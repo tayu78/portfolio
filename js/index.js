c469e8eb41642f8a7a838bef506e0bc6f6d383cd
Yuya Satake <sata.yu.7878@gmail.com>
2022-10-21 22:14:06 -0700

    changed the way of hiding hero before initial animation

--- js/index.js
+++ js/index.js
@@ -6,10 +6,14 @@ window.addEventListener("load", () => {
     .timeline()
     .to(welcomeContent, { autoAlpha: 1, duration: 3 })
     .to(welcomeContent.parentNode, { display: "none" })
-    .to(".page-container", { display: "block" })
+    .to(".page-container", { height: "auto" })
     .to("header li", { autoAlpha: 1, stagger: { each: 0.2 } })
-    .to("header h1", { autoAlpha: 1 })
-    .to("header .job-title", { autoAlpha: 1 })
-    .to("header .desc", { autoAlpha: 1 })
-    .to("header .social-list i", { autoAlpha: 1, stagger: { each: 0.2 } });
+    .to("header h1", { autoAlpha: 1, duration: 0.3 })
+    .to("header .job-title", { autoAlpha: 1, duration: 0.3 })
+    .to("header .desc", { autoAlpha: 1, duration: 0.3 })
+    .to("header .social-list i", {
+      autoAlpha: 1,
+      duration: 0.3,
+      stagger: { each: 0.2 },
+    });
 });
