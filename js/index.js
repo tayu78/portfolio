69109fc4dbd3c6541516eea241401a0f10c6774a
Yuya Satake <sata.yu.7878@gmail.com>
2022-10-24 19:23:21 -0700

    added hamburger:

--- js/index.js
+++ js/index.js
@@ -2,12 +2,28 @@ window.addEventListener("load", () => {
   const welcomeContent = document.querySelector(".welcome-content p");
   welcomeContent.textContent = "Hi, Welcome to my portfolio!";
 
+  const smallScreen = 768;
+  const isSmallScreen = window.matchMedia(
+    `(max-width: ${smallScreen}px)`
+  ).matches;
+
+  const navItems = document.querySelectorAll("header li");
+  if (isSmallScreen) {
+    navItems.forEach((item) => {
+      item.style.visibility = "visible";
+    });
+  }
+
+  const navAnimationOption = isSmallScreen
+    ? [".hamburger", { autoAlpha: 1 }]
+    : [navItem, { autoAlpha: 1, stagger: { each: 0.2 } }];
+
   gsap
     .timeline()
     .to(welcomeContent, { autoAlpha: 1, duration: 3 })
     .to(welcomeContent.parentNode, { display: "none" })
     .to(".page-container", { height: "auto" })
-    .to("header li", { autoAlpha: 1, stagger: { each: 0.2 } })
+    .to(...navAnimationOption)
     .to("header h1", { autoAlpha: 1, duration: 0.3 })
     .to("header .job-title", { autoAlpha: 1, duration: 0.3 })
     .to("header .desc", { autoAlpha: 1, duration: 0.3 })
@@ -16,4 +32,14 @@ window.addEventListener("load", () => {
       duration: 0.3,
       stagger: { each: 0.2 },
     });
+});
+
+/*
+    handle hamburger
+ */
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
 });
