cfa851df63f840b652ab8119d2a7af6b63c02d06
Yuya Satake <sata.yu.7878@gmail.com>
2023-05-03 16:03:34 -0700

    made Header responsive

--- components/Header/index..tsx
+++ components/Header/index..tsx
@@ -1,34 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import Link from "next/link";
 import styles from "./styles.module.scss";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <>
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${isActive ? styles.active : ""}`}>
         <ul>
-          <li>
-            <a href="#">Home</a>
+          <li onClick={() => setIsActive((prev) => !prev)}>
+            <Link href="#">Home</Link>
           </li>
           <li>
-            <a href="#about">About</a>
+            <Link onClick={() => setIsActive((prev) => !prev)} href="#aboutMe">
+              About
+            </Link>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <Link onClick={() => setIsActive(false)} href="#skills">
+              Skills
+            </Link>
           </li>
           <li>
-            <a href="#blogs">Blogs</a>
+            <Link onClick={() => setIsActive(false)} href="#projects">
+              Projects
+            </Link>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <Link onClick={() => setIsActive(false)} href="#blogs">
+              Blogs
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setIsActive(false)} href="#contacts">
+              Contacts
+            </Link>
           </li>
         </ul>
       </nav>
-      <div className="hamburger">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
+      <div
+        className={`${styles.burgerMenu} ${isActive ? styles.active : ""}`}
+        onClick={() => setIsActive((prev) => !prev)}
+      >
+        <span className={styles.burgerBar}></span>
+        <span className={styles.burgerBar}></span>
+        <span className={styles.burgerBar}></span>
       </div>
     </>
   );
